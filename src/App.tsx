e8c00de0d30348f0c1f7dfb211b2062e1be40a2b
Raon98 <cheol1998@kakao.com>
2023-10-30 18:48:14 +0900

    login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Router from "./component/Router";
 import {app} from  "firebaseApp"
-import {getAuth} from "firebase/auth"
+import {getAuth, onAuthStateChanged} from "firebase/auth"
 import {ToastContainer, toast} from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
+import Loader from "component/Loader";
 function App() {
 
     const auth = getAuth(app);
-    // firebase 의 Auth가 인증되었으면  true로 변경해주는 로직 추가
+
+    //auth를 체크하기전에 (initialize 전)에 loader를 띄어주는 용도
+    const [init,setInit] = useState<boolean>(false)
+    // auth에 current User가 있으면 authenticated로 ㅂ녀경
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!auth?.currentUser)
 
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if(user){
+                setIsAuthenticated(true)
+            }else {
+                setIsAuthenticated(false)
+            }
+            setInit(true)
+        })
+    },[auth])
     return (
         <>
             <ToastContainer/>
-            <Router isAuthenticated={isAuthenticated}/>
-
+            {init ? <Router isAuthenticated={isAuthenticated}/> : <Loader/>}
         </>
     )
 }
--- src/component/LoginForm.tsx
+++ src/component/LoginForm.tsx
@@ -1,23 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
+import {toast} from "react-toastify";
+import {app} from "firebaseApp"
+import {getAuth,signInWithEmailAndPassword} from "firebase/auth";
 
 const LoginForm = () => {
+    const [error, setError] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const onSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();  //기본동작 제어
+
+        try {
+            const auth = getAuth();
+            await signInWithEmailAndPassword(auth,email,password)
+
+            toast.success("로그인에 성공했습니다.")
+        }catch (error:any) {
+            toast.error(error?.code)
+            console.log(error)
+        }
+    }
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {target: {name, value}} = e;
+
+        if (name === 'email') {
+            setEmail(value)
+            const validRegex =
+                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+            if (!value?.match(validRegex)) {
+                setError("이메일 형식이 올바르지 않습니다.");
+            } else {
+                setError("");
+            }
+        }
+        if (name === 'password') {
+            setPassword(value)
+
+            if (value?.length < 8) {
+                setError("비밀번호는 8자리 이상 입력해주세요.");
+            } else {
+                setError("");
+            }
+        }
+    }
     return (
-        <form action="/post" method="POST" className="form form--lg">
+        <form onSubmit={onSubmit} className="form form--lg">
             <h1 className="form__title">로그인</h1>
             <div className="form__block">
                 <label htmlFor="email">이메일</label>
-                <input type="email" name="email" id="email" required/>
+                <input type="email" name="email" id="email" value={email} required onChange={onChange}/>
             </div>
             <div className="form__block">
                 <label htmlFor="password">비밀번호</label>
-                <input type="password" name="password" id="password" required/>
+                <input type="password" name="password" id="password" value={password} required onChange={onChange}/>
             </div>
+            {error && error?.length > 0 && (
+                <div className="form__block">
+                    <div className="form__error">{error}</div>
+                </div>
+            )}
             <div className="form__block">
                 계정이 없으신가요? <Link to="/signup" className="form__link">회원가입하기</Link>
             </div>
             <div className="form__block">
-                <input type="submit" value="로그인" className="form__btn--submit"/>
+                <input type="submit" value="로그인" className="form__btn--submit" disabled={error?.length > 0}/>
             </div>
         </form>
     );
