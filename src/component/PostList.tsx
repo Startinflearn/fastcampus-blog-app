9a013a7c27845905c1b096c3cc39b09c0321b37b
Raon98 <cheol1998@kakao.com>
2023-11-07 20:01:14 +0900

    Blog APP Project Ch.05__04. Firestore로 데이터 가져오기 게시글 리스트 작업

--- src/component/PostList.tsx
+++ src/component/PostList.tsx
@@ -1,14 +1,41 @@
 import {Link} from "react-router-dom";
 
-import React, {useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
+import {collection, getDocs} from "firebase/firestore";
+import {db} from "firebaseApp";
+import AuthContext from "context/AuthContext";
 
 interface PostListProps {
     hasNavigation?: boolean
 }
 
 type TabType = "all" | 'my'
+
+interface PostProps {
+    id: string,
+    title: string,
+    email: string,
+    summary: string,
+    content: string,
+    createdAt: string
+}
+
 export default function PostList({hasNavigation = true}: PostListProps) {
-    const [activeTab, setActiveTab] = useState<TabType>("all")
+    const [activeTab, setActiveTab] = useState<TabType>("all");
+    const [posts, setPosts] = useState<PostProps[]>([]);
+    const {user} = useContext(AuthContext);
+
+    const getPosts = async () => {
+        const datas = await getDocs(collection(db, "posts"));
+
+        datas?.forEach((doc) => {
+            const dataObj = {...doc.data(), id: doc.id}
+            setPosts((prev) => [...prev, dataObj as PostProps])
+        })
+    }
+    useEffect(() => {
+        getPosts()
+    }, [])
     return <>
         {
             hasNavigation && (
@@ -23,34 +50,31 @@ export default function PostList({hasNavigation = true}: PostListProps) {
             )
         }
         <div className="post__list">
-            {[
-                ...Array(10)].map((e, idx: number) => (
-                <div key={idx} className="post__box">
-                    <Link to={`/posts/${idx}`}>
-                        <div className="post__profile-box">
-                            <div className="post__profile"/>
-                            <div className="post__author-name">패스트캠퍼스</div>
-                            <div className="post__date">2023.07.08 토요일</div>
+            {
+                posts?.length > 0 ? posts.map((post, idx: number) => (
+                        <div key={post?.id} className="post__box">
+                            <Link to={`/posts/${post?.id}`}>
+                                <div className="post__profile-box">
+                                    <div className="post__profile"/>
+                                    <div className="post__author-name">{post?.email}</div>
+                                    <div className="post__date">{post?.createdAt}</div>
+
+                                </div>
+                                <div className="post__title">{post?.title}</div>
+                                <div className="post_text">{post?.content}</div>
+                            </Link>
+                                {post.email === user?.email && (
+                                    <div className="post__utils-box">
+                                        <div className="post__delete">삭제</div>
+                                        <div className="post__edit">
+                                            <Link to={`/posts/edit/${post?.id}`}>수정</Link>
+                                        </div>
+                                    </div>
+                                )}
+
                         </div>
-                        <div className="post__title">
-                            게시글 {idx}
-                        </div>
-                        <div className="post_text">
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                            mollit anim id est laborum."
-                        </div>
-                        <div className="post__utils-box">
-                            <div className="post__delete">삭제</div>
-                            <div className="post__edit">수정</div>
-                        </div>
-                    </Link>
-                </div>
-            ))
-            }
+                    ))
+                    : <div className="post__no-post">게시글이 없습니다.</div>}
         </div>
     </>
 }
