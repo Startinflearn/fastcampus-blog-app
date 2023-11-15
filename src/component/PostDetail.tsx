c8bf2df1cd2b3942a5d3a3f6b108a4cad3fb1c55
Raon98 <cheol1998@kakao.com>
2023-11-15 18:39:39 +0900

    Firestore로 데이터 삭제하기 게시글 삭제 구현

--- src/component/PostDetail.tsx
+++ src/component/PostDetail.tsx
@@ -1,26 +1,33 @@
 import React, {useEffect, useState} from "react";
-import {Link, useParams} from "react-router-dom";
+import {Link, useNavigate, useParams} from "react-router-dom";
 import { PostProps } from"./PostList"
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc,deleteDoc } from "firebase/firestore";
 import {db} from "firebaseApp";
 import Loader from "component/Loader";
+import {toast} from "react-toastify";
 
 export default function PostDetail() {
     const [post, setPost] = useState<PostProps | null>(null)
     const params = useParams();
-
+    const navigate = useNavigate();
     const getPost = async (id : string) => {
         if(id){
             const docRef = doc(db, 'posts', id);
             const docSnap = await getDoc(docRef);
 
-
             setPost({id:docSnap.id , ...(docSnap.data() as PostProps)})
         }
     }
 
-    const handleDelete = () => {
-        console.log('delete')
+    const handleDelete = async () => {
+
+        const confirm = window.confirm('해당 게시글을 삭제하시겠습니까?')
+        if(confirm && post && post.id){
+            await deleteDoc(doc(db,"posts",post.id))
+            toast.success('게시글을 삭제했습니다.')
+            navigate("/");
+        }
+
     }
     useEffect(()=> {
         if(params?.id) getPost(params?.id);
