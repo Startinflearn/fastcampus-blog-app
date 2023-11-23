7f0ade7838bd04c4c39b3f0e40a5369a6d6d4577
Raon98 <cheol1998@kakao.com>
2023-11-23 18:23:31 +0900

    다크모드 toggle 기능 구현

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,12 +1,22 @@
 import {Link} from "react-router-dom";
-import React from "react";
+import React, {useContext} from "react";
+import {BsSun, BsMoonFill} from 'react-icons/bs'
+import ThemeContext from "context/ThemeContext";
 
 export default function Footer() {
+    const context = useContext(ThemeContext);
+
+
     return (
-            <footer className="footer">
-                <Link to="/posts/new">글쓰기</Link>
-                <Link to="/posts">게시글</Link>
-                <Link to="/profile">프로필</Link>
-            </footer>
+        <footer className="footer">
+            <Link to="/posts/new">글쓰기</Link>
+            <Link to="/posts">게시글</Link>
+            <Link to="/profile">프로필</Link>
+
+            <>
+                {context.theme === 'light' ? <BsSun className="footer__theme-btn" onClick={context.toggleMode}/> :
+                    <BsMoonFill className="footer__theme-btn" onClick={context.toggleMode}/>}
+            </>
+        </footer>
     )
 }
